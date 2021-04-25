931b882d73c1313be3721fe4ce8e6d1e61b1e7d3
yousuf4you <yousuf360.ya@gmail.com>
2021-04-25 20:07:05 +0600

    Some edit:

--- src/components/NavBar/AppBar.jsx
+++ src/components/NavBar/AppBar.jsx
@@ -1,23 +1,20 @@
 import React from "react";
+import { Link } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import MoreIcon from "@material-ui/icons/MoreVert";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 import useStyles from "./appbarStyles";
 import logo from "../../assets/image/tvs-motor-logo.svg";
-import { Link } from "@material-ui/core";
 
 const NavBar = () => {
 	const classes = useStyles();
@@ -54,6 +51,7 @@ const NavBar = () => {
 							<li className={classes.anchor}>Motors</li>
 							<li className={classes.anchor}>Parts</li>
 							<li className={classes.anchor}>Servicing</li>
+							<li className={classes.anchor}>Sells</li>
 						</ul>
 					</div>
 
